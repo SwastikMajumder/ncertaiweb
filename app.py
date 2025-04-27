86300c2e9a5a7a52af012a5a406b486b86ae7fc4
SwastikMajumder <75435931+SwastikMajumder@users.noreply.github.com>
2025-04-27 19:05:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -274,4 +274,4 @@ def serve_images(filename):
     return send_from_directory('static/images', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
